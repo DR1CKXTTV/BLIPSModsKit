6cee2082f90bc70ba7a36bdb83ceccc7eb6e5183
DR1CKXTTV <55298136+DR1CKXTTV@users.noreply.github.com>
2019-12-30 18:19:28 -0500

    Update MBlipsDR1.lua

--- BLIPSDR1CKX/MBlipsDR1.lua
+++ BLIPSDR1CKX/MBlipsDR1.lua
@@ -1,8 +1,8 @@
 Config = {}
 
 Config.Map = {
-  {name="~b~Maison ~r~Derek ~w~& ~r~Brian",color=59, id=492, x = -2610.03,  y = 1692.92,  z = 142.66474914551},
-  --{name="~b~Maison ~y~Rock Norris",color=0, id=206, x = 956.657, y = 447.009, z = 122.800},
+  {name="~b~Maison ~r~Derek",color=59, id=492, x = -2610.03,  y = 1692.92,  z = 142.66474914551},
+  --{name="~b~Maison ~y~Richard",color=0, id=206, x = 956.657, y = 447.009, z = 122.800},
   {name="~b~Maison Lucifer",color=0, id=123, x = 1355.29, y = -708.98, z = 67.27},
    
  }
