3714afb87ed36d445083c25f161b9602c505f848
DR1CKXTTV <55298136+DR1CKXTTV@users.noreply.github.com>
2019-12-30 18:18:12 -0500

    Add files via upload

---
+++ BLIPSDR1CKX/MBlipsDR1.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+Config.Map = {
+  {name="~b~Maison ~r~Derek ~w~& ~r~Brian",color=59, id=492, x = -2610.03,  y = 1692.92,  z = 142.66474914551},
+  --{name="~b~Maison ~y~Rock Norris",color=0, id=206, x = 956.657, y = 447.009, z = 122.800},
+  {name="~b~Maison Lucifer",color=0, id=123, x = 1355.29, y = -708.98, z = 67.27},
+   
+ }
+
+Citizen.CreateThread(function()
+	
+	for i=1, #Config.Map, 1 do
+		
+		local blip = AddBlipForCoord(Config.Map[i].x, Config.Map[i].y, Config.Map[i].z)
+		SetBlipSprite (blip, Config.Map[i].id)
+		SetBlipScale  (blip, 1.4)
+		SetBlipDisplay(blip, 4)
+		SetBlipColour (blip, Config.Map[i].color)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(Config.Map[i].name)
+		EndTextCommandSetBlipName(blip)
+	end
+end)
---
+++ BLIPSDR1CKX/__resource.lua
@@ -0,0 +1,15 @@
+     ----------------------------------------------------------------
+     ----------------------------------------------------------------
+     ----##© Dev of first script--> BLIPS : DR1CKXTTV on GITHUB ©----
+     ----------------------------------------------------------------
+     ----------------------------------------------------------------
+     
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Show Blips On Map. Made by DR1CKXTTV'
+
+version '0.1.0'
+
+client_scripts {
+	'MBlipsDR1.lua'
+}
